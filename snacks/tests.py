d1790b4551f5ea231cb94dd575ec72c3f32543f2
Joe Streifel <josephstreifel@gmail.com>
2022-01-05 15:12:56 -0800

    adds required tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class SnacksTestCase(SimpleTestCase):
+    def _get_url(self, name):
+        url = reverse(name)
+        response = self.client.get(url)
+        return response
+
+    def test_home_status_code(self):
+        res = self._get_url('home')
+        self.assertEqual(res.status_code, 200)
+    
+    def test_home_template(self):
+        res = self._get_url('home')
+        self.assertTemplateUsed(res, 'home.html')
+        self.assertTemplateUsed(res, '_base.html')
+
+    def test_about_status_code(self):
+        res = self._get_url('about')
+        self.assertEqual(res.status_code, 200)
+
+    def test_about_template(self):
+        res = self._get_url('about')
+        self.assertTemplateUsed(res, 'about.html')
+        self.assertTemplateUsed(res, '_base.html')
